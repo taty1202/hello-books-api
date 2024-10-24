03ffc6e60153c2255ca2641a585ff5bc10926db2
Tatyana Venanzi <tatyanavenanzi@gmail.com>
2024-10-24 16:29:53 -0700

    set up models in flask app and set up sqlalchemy

--- app/models/book.py
+++ app/models/book.py
@@ -1,10 +1,18 @@
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
     
-    def to_book(self):
+    def to_dict(self):
         return dict(
                 id=self.id,
                 title=self.title,
@@ -12,8 +20,8 @@ class Book:
             
         )
 
-books = [
-    Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
-    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-]
+# books = [
+#     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
+#     Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
+#     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+# ]
--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,41 +1,62 @@
-from flask import Blueprint, abort, make_response
-from app.models.book import books
+from flask import Blueprint, abort, make_response, request
+from app.models.book import Book
+from ..db import db
 
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
+@books_bp.post("")
+def create_book():
+    request_body = request.get_json()
+    title = request_body["title"]
+    description = request_body["description"]
+
+    new_book = Book(title=title, description=description)
+    db.session.add(new_book)
+    db.session.commit()
+
+    response = {
+        "id": new_book.id,
+        "title": new_book.title,
+        "description": new_book.description,
+    }
+    return response, 201
+
 @books_bp.get("")
 def get_all_books():
+    query = db.select(Book).order_by(Book.id)
+    books = db.session.scalars(query)
+
     books_response = []
     for book in books:
-        books_response.append(book.to_book())
+        books_response.append(book.to_dict())
     return books_response
 
-@books_bp.get("/<book_identifier>")
-def get_one_book(book_identifier):
-    book = validate_book_identifier(book_identifier)
-    return book.to_book(book_identifier)
+# @books_bp.get("/<book_identifier>")
+# def get_one_book(book_identifier):
+#     book = validate_book_identifier(book_identifier)
+#     return book.to_dict(book_identifier)
 
-def validate_book_identifier(book_identifier):
-    if book_identifier.isdigit():
-        return validate_book_id(book_identifier)
-    else:
-        return validate_book_name(book_identifier)
+# def validate_book_identifier(book_identifier):
+#     if book_identifier.isdigit():
+#         return validate_book_id(book_identifier)
+#     else:
+#         return validate_book_name(book_identifier)
     
-def validate_book_id(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        abort(make_response({"message": f"book id {book_id} invalid"}, 400))
+# def validate_book_id(book_id):
+#     try:
+#         book_id = int(book_id)
+#     except:
+#         abort(make_response({"message": f"book id {book_id} invalid"}, 400))
 
-    for book in books:
-        if book.id == book_id:
-            return book
-    abort(make_response({"message": f"book id {book_id} invalid"}, 404))
+#     for book in books:
+#         if book.id == book_id:
+#             return book
+#     abort(make_response({"message": f"book id {book_id} invalid"}, 404))
 
 
-def validate_book_name(book_name):
-    for book in books:
-        if book.name.lower() == book_name.lower():
-            return book
-    abort(make_response({"message": f"book id {book_name} invalid"}, 404))
+# def validate_book_name(book_name):
+#     for book in books:
+#         if book.name.lower() == book_name.lower():
+#             return book
+#     abort(make_response({"message": f"book id {book_name} invalid"}, 404))
